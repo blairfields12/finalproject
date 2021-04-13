63f3044d5937e1898137e15f9f52d66e29f4dda2
Mia Schaftel <miaschaf@umich.edu>
2021-04-13 17:29:06 -0400

    first april 13, 5:29pm

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -1,1 +1,24 @@
 #Weather API Data
+
+import json
+import requests
+import sqlite3
+
+API_KEY = '2c4debeef933141f65cb3c162b82970d'
+
+annarbor_latitude = '42.280826'
+annarbor_longitude = '-83.743038'
+
+la_latitude = '34.052234'
+la_longitude = '-118.243685'
+
+def weather_data(API_KEY, latitude, longitude):
+    baseurl = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(latitude, longitude, API_KEY)
+    r = requests.get(baseurl)
+    response = json.loads(r.text)
+    data = []
+
+    for i in range():
+        data.append(response[''])
+    
+    return data
