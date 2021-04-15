d40bb933130d4b50365eb7f16acbeed687d6a805
Blair Fields <blairfields@Blairs-MacBook-Pro.local>
2021-04-15 12:44:01 -0400

    updates

--- YelpAPIData.py
+++ YelpAPIData.py
@@ -3,3 +3,13 @@
 import json
 import requests
 import sqlite3
+
+apiKey = 'mq7UhVeyQ6mG3od9DRFjZQ'
+
+def dataFromYelp(apiKey, location):
+    baseURL = 'https://api.yelp.com/v3/businesses/search'
+    headers = {'Authorization': 'Bearer %s' % apiKey}
+    p = {'term' : 'restaurant', 'location' : location, 'sort_by' : 'review_count', 'sort_by' : 'rating', 'limit' : 50}
+    requestURL = requests.get(baseURL, headers = headers, params = p)
+    
+
