8c51908068785aeeadb496e7fac674070976ce94
Mia Schaftel <miaschaf@umich.edu>
2021-04-19 16:58:19 -0400

    newnewnew

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -35,22 +35,26 @@ def weather_data(API_KEY, latitude, longitude, start_date):
     data = []
 
     position = 0
-    for elem in response['hourly']:
-        time = response['hourly'][position]['dt']
-        city = response['lat']
-        if city == 42.2808:
-            city_name = 'Ann Arbor'
-            temp = response['hourly'][position]['temp']
-            forecast = response['hourly'][position]['weather'][0]['description']
-            humidity = response['hourly'][position]['humidity']
-        if city == 34.0522:
-            city_name = 'Los Angeles'
-            temp = response['hourly'][position]['temp']
-            forecast = response['hourly'][position]['weather'][0]['description']
-            humidity = response['hourly'][position]['humidity']
-        data.append((time, city_name, temp, forecast, humidity))
-        position = position + 1
-
+   
+    try:
+        for elem in response['hourly']:
+            time = response['hourly'][position]['dt']
+            city = response['lat']
+            if city == 42.2808:
+                city_name = 'Ann Arbor'
+                temp = response['hourly'][position]['temp']
+                forecast = response['hourly'][position]['weather'][0]['description']
+                humidity = response['hourly'][position]['humidity']
+            if city == 34.0522:
+                city_name = 'Los Angeles'
+                temp = response['hourly'][position]['temp']
+                forecast = response['hourly'][position]['weather'][0]['description']
+                humidity = response['hourly'][position]['humidity']
+            data.append((time, city_name, temp, forecast, humidity))
+            position = position + 1
+    except:
+        print('ERROR')
+        print(response['hourly'])
 
     #print(data)
     return data
@@ -61,22 +65,25 @@ def create_table(cur, conn, data):
     sorted information from both cities to the table.'''
 
     #cur.execute('DROP TABLE IF EXISTS WeatherData')
-    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (time INTEGER PRIMARY KEY, city TEXT, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (id INTEGER PRIMARY KEY, time INTEGER, city TEXT, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
+    cur.execute("SELECT * FROM WeatherData")
+    num = len(cur.fetchall())
+    #print(len(data))
     for elem in data:
-        cur.execute("INSERT INTO WeatherData (time, city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?, ?)", (elem[0], elem[1], elem[2], elem[3], elem[4]))
-
+        cur.execute("INSERT INTO WeatherData (id, time, city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?, ?, ?)", (num, elem[0], elem[1], elem[2], elem[3], elem[4]))
+        num = num + 1
     conn.commit()
 
 def main():
     cur, conn = setUpDatabase('WeatherData.db')
     start_date = 1618358400
     
-    for i in range(101):
+    for i in range(0, 25):
         AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
         LA = weather_data(API_KEY, la_latitude, la_longitude, start_date)
         create_table(cur, conn, AA)
         create_table(cur, conn, LA)
-        start_date = start_date + 3,600
+        start_date = start_date + 3600
 
 if __name__ == "__main__":
     main()
