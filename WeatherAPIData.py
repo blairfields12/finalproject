d34c833c4d3b90a37d58b0268788fdc65dc7b6fe
miascha <78377452+miascha@users.noreply.github.com>
2021-04-18 23:05:35 -0400

    update 04/18 11:05

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -13,8 +13,6 @@ annarbor_longitude = -83.7430
 la_latitude = 34.0522
 la_longitude = -118.2437
 
-start_date = 1618358400
-
 
 def setUpDatabase(db_name):
     '''This function will create a database named after the string 
@@ -31,6 +29,7 @@ def weather_data(API_KEY, latitude, longitude, start_date):
     baseurl = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={}&lon={}&dt={}&appid={}".format(latitude, longitude, start_date, API_KEY)
     r = requests.get(baseurl)
     response = json.loads(r.text)
+    print(response)
     data = []
 
     for elem in response:
@@ -51,16 +50,18 @@ def create_table(cur, conn, data):
     '''This function creates the WeatherData table and inserts the 
     sorted information from both cities to the table.'''
 
-    cur.execute("DROP TABLE IF EXISTS WeatherData")
+    #cur.execute("DROP TABLE IF EXISTS WeatherData")
     cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (city TEXT PRIMARY KEY, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
     for elem in data:
-        print(elem)
-        cur.execute("INSERT INTO WeatherData (city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?)", (elem[0], elem[1], elem[2], elem[3]))
+        cur.execute("INSERT OR IGNORE INTO WeatherData (city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?)", (elem[0], elem[1], elem[2], elem[3]))
     conn.commit()
 
 def main():
     cur, conn = setUpDatabase('weather_data.db')
-    for i in range(100):
+    start_date = 1618358400
+    # AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
+    # create_table(cur, conn, AA)
+    for i in range(25):
         AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
         LA = weather_data(API_KEY, la_latitude, la_longitude, start_date)
         create_table(cur, conn, AA)
@@ -69,25 +70,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# def sort_by_city(API_KEY, latitude, longitude, start_date):
-#     '''This function sorts all the data based on which city is input and
-#     returns a list containing the weather data from one city.'''
-#     city = weather_data(API_KEY, latitude, longitude, start_date)
-#     city_sort = []
-
-#     for elem in city:
-#         temp = city['current']['temp']
-#         forecast = city['current']['weather'][0]['description']
-#         humidity = city['current']['humidity']
-
-#         if city['lat'] == annarbor_latitude:
-#             city_sort.append(('AA', temp, forecast, humidity))
-#         if city['lat'] == la_latitude:
-#             city_sort.append(('LA', temp, forecast, humidity))
-#     print(city_sort)
-#     return city_sort
