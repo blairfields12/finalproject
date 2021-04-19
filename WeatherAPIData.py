513eea991c4118699ac79afa692d9276b4f1028b
Mia Schaftel <miaschaf@umich.edu>
2021-04-19 17:08:56 -0400

    more progress

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -64,26 +64,23 @@ def create_table(cur, conn, data):
     '''This function creates the WeatherData table and inserts the 
     sorted information from both cities to the table.'''
 
-    #cur.execute('DROP TABLE IF EXISTS WeatherData')
-    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (id INTEGER PRIMARY KEY, time INTEGER, city TEXT, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
-    cur.execute("SELECT * FROM WeatherData")
-    num = len(cur.fetchall())
-    #print(len(data))
-    for elem in data:
-        cur.execute("INSERT INTO WeatherData (id, time, city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?, ?, ?)", (num, elem[0], elem[1], elem[2], elem[3], elem[4]))
-        num = num + 1
+    cur.execute('DROP TABLE IF EXISTS WeatherData')
+    # cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (id INTEGER PRIMARY KEY, time INTEGER, city TEXT, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
+    # cur.execute("SELECT * FROM WeatherData")
+    # num = len(cur.fetchall())
+
+    # for elem in data:
+    #     cur.execute("INSERT INTO WeatherData (id, time, city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?, ?, ?)", (num, elem[0], elem[1], elem[2], elem[3], elem[4]))
+    #     num = num + 1
     conn.commit()
 
 def main():
     cur, conn = setUpDatabase('WeatherData.db')
     start_date = 1618358400
-    
-    for i in range(0, 25):
-        AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
-        LA = weather_data(API_KEY, la_latitude, la_longitude, start_date)
-        create_table(cur, conn, AA)
-        create_table(cur, conn, LA)
-        start_date = start_date + 3600
+    AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
+    LA = weather_data(API_KEY, la_latitude, la_longitude, start_date)
+    create_table(cur, conn, AA)
+    create_table(cur, conn, LA)
 
 if __name__ == "__main__":
     main()
