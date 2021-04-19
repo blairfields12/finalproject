1cc3ee0259583b347ad7ad841b849a5fca75b724
Mia Schaftel <miaschaf@umich.edu>
2021-04-19 17:55:12 -0400

    update 555

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -59,6 +59,21 @@ def weather_data(API_KEY, latitude, longitude, start_date):
                 temp = response['hourly'][position]['temp']
                 forecast = response['hourly'][position]['weather'][0]['description']
                 humidity = response['hourly'][position]['humidity']
+            if city == 41.8781:
+                city_name = 'Chicago'
+                temp = response['hourly'][position]['temp']
+                forecast = response['hourly'][position]['weather'][0]['description']
+                humidity = response['hourly'][position]['humidity']
+            if city == 42.3314:
+                city_name = 'Detroit'
+                temp = response['hourly'][position]['temp']
+                forecast = response['hourly'][position]['weather'][0]['description']
+                humidity = response['hourly'][position]['humidity']
+            if city == 40.7128:
+                city_name = 'New York City'
+                temp = response['hourly'][position]['temp']
+                forecast = response['hourly'][position]['weather'][0]['description']
+                humidity = response['hourly'][position]['humidity']
             data.append((time, city_name, temp, forecast, humidity))
             position = position + 1
     except:
@@ -72,15 +87,15 @@ def create_table(cur, conn, data):
     '''This function creates the WeatherData table and inserts the 
     sorted information from both cities to the table.'''
 
-    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (id INTEGER PRIMARY KEY, time INTEGER, city TEXT, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (ID INTEGER PRIMARY KEY, Time INTEGER, City TEXT, Temperature FLOAT, Forecast TEXT, Humidity_Percentage FLOAT)")
     cur.execute("SELECT * FROM WeatherData")
     num = len(cur.fetchall())
     count = 0
     for elem in data:
         if count == 5:
             break
-        else:
-            cur.execute("INSERT INTO WeatherData (id, time, city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?, ?, ?)", (num, elem[0], elem[1], elem[2], elem[3], elem[4]))
+        if cur.execute("SELECT Time FROM WeatherData WHERE Time = ?", (elem[0],)).fetchone() == None:
+            cur.execute("INSERT INTO WeatherData (ID, Time, City, Temperature, Forecast, Humidity_Percentage) VALUES (?, ?, ?, ?, ?, ?)", (num, elem[0], elem[1], elem[2], elem[3], elem[4]))
             num = num + 1
             count = count + 1
 
@@ -89,11 +104,13 @@ def create_table(cur, conn, data):
 def main():
     cur, conn = setUpDatabase('WeatherData.db')
     start_date = 1618358400
+    
     AA = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
     LA = weather_data(API_KEY, la_latitude, la_longitude, start_date)
     CHI = weather_data(API_KEY, chi_latitude, chi_longitude, start_date)
     DET = weather_data(API_KEY, det_latitude, det_longitude, start_date)
     NYC = weather_data(API_KEY, nyc_latitude, nyc_latitude, start_date)
+    
     create_table(cur, conn, AA)
     create_table(cur, conn, LA)
     create_table(cur, conn, CHI)
