7856c3ac9ec92ff9071b9f808ad74f5801bff247
miascha <78377452+miascha@users.noreply.github.com>
2021-04-18 22:30:33 -0400

    update 04/18 10:30 pm

--- WeatherAPIData.py
+++ WeatherAPIData.py
@@ -7,14 +7,14 @@ import os
 
 API_KEY = '2c4debeef933141f65cb3c162b82970d'
 
-annarbor_latitude = 42.280826
-annarbor_longitude = -83.743038
+annarbor_latitude = 42.2808
+annarbor_longitude = -83.7430
 
-la_latitude = 34.052234
-la_longitude = -118.243685
+la_latitude = 34.0522
+la_longitude = -118.2437
 
-start_date = 1614574800
-end_date = 1617249599
+start_date = 1618358400
+
 
 def setUpDatabase(db_name):
     '''This function will create a database named after the string 
@@ -24,39 +24,68 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def weather_data(API_KEY, latitude, longitude):
+def weather_data(API_KEY, latitude, longitude, start_date):
     '''This function calls the requests.get() method on the OpenWeather 
-    link and creates a list of all the data for both cities.'''
-    baseurl = "http://history.openweathermap.org/data/2.5/history/city?lat={}&lon={}&type=hour&start={}&end={}&appid={}".format(latitude, longitude, start_date, end_date, API_KEY)
+    link and creates a list of tuples containing the temperature, forecast, 
+    humidity, and weather conditions for each citiy at a given unix time.'''
+    baseurl = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={}&lon={}&dt={}&appid={}".format(latitude, longitude, start_date, API_KEY)
     r = requests.get(baseurl)
     response = json.loads(r.text)
     data = []
 
-    for i in response:
-        data.append(response[])
+    for elem in response:
+        city = response['lat']
+        if city == 42.2808:
+            city_name = 'Ann Arbor'
+        if city == 34.0522:
+            city_name = 'Los Angeles'
+        temp = response['current']['temp']
+        forecast = response['current']['weather'][0]['description']
+        humidity = response['current']['humidity']
+        data.append((city_name, temp, forecast, humidity))
+
     return data
 
-def sort_by_city(API_KEY, latitude, longitude):
-    '''This function sorts all the data based on which city is input and
-    returns a list containing the weather data from one city.'''
-    city = weather_data(API_KEY, latitude, longitude)
-    city_sort = []
 
-    for elem in city:
-        if city['latitude'] == annarbor_latitude:
-            city_sort.append('AA')
-        if city['latitude'] == la_latitude:
-            city_sort.append('LA')
-    return city_sort
-
-def create_table():
+def create_table(cur, conn, data):
     '''This function creates the WeatherData table and inserts the 
     sorted information from both cities to the table.'''
-    cur.execute("CREATE TABLE IF NOT EXISTS Weather Data (city TEXT PRIMARY KEY, date INTEGER, temperature FLOAT, forecast TEXT)")
+
+    cur.execute("DROP TABLE IF EXISTS WeatherData")
+    cur.execute("CREATE TABLE IF NOT EXISTS WeatherData (city TEXT PRIMARY KEY, temperature FLOAT, forecast TEXT, humidity_percentage FLOAT)")
+    for elem in data:
+        print(elem)
+        cur.execute("INSERT INTO WeatherData (city, temperature, forecast, humidity_percentage) VALUES (?, ?, ?, ?)", (elem[0], elem[1], elem[2], elem[3]))
+    conn.commit()
 
 def main():
     cur, conn = setUpDatabase('weather_data.db')
-    weather_data(API_KEY, annarbor_latitude, annarbor_longitude)
+    AA1 = weather_data(API_KEY, annarbor_latitude, annarbor_longitude, start_date)
+    create_table(cur, conn, AA1)
+    # for i in range(100):
+    #     start_date = start_date + 3,600
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+# def sort_by_city(API_KEY, latitude, longitude, start_date):
+#     '''This function sorts all the data based on which city is input and
+#     returns a list containing the weather data from one city.'''
+#     city = weather_data(API_KEY, latitude, longitude, start_date)
+#     city_sort = []
+
+#     for elem in city:
+#         temp = city['current']['temp']
+#         forecast = city['current']['weather'][0]['description']
+#         humidity = city['current']['humidity']
+
+#         if city['lat'] == annarbor_latitude:
+#             city_sort.append(('AA', temp, forecast, humidity))
+#         if city['lat'] == la_latitude:
+#             city_sort.append(('LA', temp, forecast, humidity))
+#     print(city_sort)
+#     return city_sort
