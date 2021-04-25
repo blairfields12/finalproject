d3241743cd29e41e0f6ca018e6f3cd238113a91d
Blair Fields <blairfields@Blairs-MacBook-Pro.local>
2021-04-25 15:38:51 -0400

    I think done

--- citiesCalculations.py
+++ citiesCalculations.py
@@ -16,43 +16,43 @@ def salary_quality():
     data = cur.fetchall()
     conn.commit()
 
-    salaries = []
+    cityNames = []
+    income = []
     qualityOfLife = []
     population = [] 
     for tup in data: 
         # print(tup)
+        cityNames.append(tup[0])
         population.append(tup[1])
-        salaries.append(tup[2])
+        income.append(tup[2])
         qualityOfLife.append(tup[3])
         # for i in tup: 
         #     info.append(i)
     # print("population: " + str(population))
     # print("::::::::::::")
-    # print("salaries: " + str(salaries))
+    # print("income: " + str(income))
     # print(":::::::::::::")
     # print("qual of life: " + str(qualityOfLife))
 
-    MoneyPerPersonn = [] 
-    for i in range(len(population)): 
-        moneyperperson = population[i]/salaries[i]
-        MoneyPerPersonn.append(moneyperperson)
-    print(MoneyPerPersonn)
+    """Trying to calculate if there is a relationship between each cities income and their quality of life rating"""
+    relationships = []
+    for i in range(len(income)): 
+        calc = income[i]/qualityOfLife[i]
+        relationships.append([cityNames[i], calc])
+    # print(relationships)
+
 
 
     with open('CitiesInformation.csv', 'w', newline = '', encoding= 'utf-8') as f: 
         f = csv.writer(f, delimiter = ',')
-        f.writerow(['Populations', 'Salaries', 'Quality of Life', 'Money Per Person'])
+        f.writerow(['City Name', 'Populations', 'Avg Income', 'Quality of Life', 'Avg Income / Quality of Life'])
         count = 0
         for i in population:
-            headers = (population[count], salaries[count], qualityOfLife[count], MoneyPerPersonn[count])
+            headers = (cityNames[count], population[count], income[count], qualityOfLife[count], relationships[count][1])
             count += 1
-
-
-
-        # headers = (population, salaries, qualityOfLife, MoneyPerPersonn)
             f.writerow(headers)
-
+    
 
 #socioeconomic status for each city to see if restaurants that are pricer have more wealthy people in that city 
-
 salary_quality()
+
