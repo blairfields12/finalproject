d07642b9b372e84a88fd868afc5af1eeb1645e96
Blair Fields <blairfields@Blairs-MacBook-Pro.local>
2021-04-25 12:38:43 -0400

    trying

--- citiesVisualization.py
+++ citiesVisualization.py
@@ -1,1 +1,24 @@
+#cities visualization 
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+import sqlite3
+import csv
+
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(base_dir, '/Users/blairfields/Desktop/SI206/finalprojectfolder/finalproject/finalprojectdatabase.db')
+
+with sqlite3.connect(path) as Yelpdatabase:
+    cur = Yelpdatabase.cursor()
+    cur.execute("SELECT * FROM CitiesData")
+    data = cur.fetchall()
+    print(data)
+
+    
+
 
