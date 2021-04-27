7c1aa5e6e343ec96bcdf2e3e7c87532e8761b10d
Mia Schaftel <miaschaf@umich.edu>
2021-04-27 15:25:38 -0400

    new

--- CitiesJoinYelp.py
+++ CitiesJoinYelp.py
@@ -8,7 +8,7 @@ def joinCitiesData():
     conn = sqlite3.connect(base_dir + '/finalprojectdatabase.db')
     cur = conn.cursor()
     # cur.execute("SELECT CitiesData.Quality_of_Life, CitiesData.Average_Annual_Salary, CitiesData.Population FROM CitiesData LEFT JOIN RestaurantCities ON RestaurantCities.ID = CitiesData.CityID")
-    cur.execute("SELECT Quality_of_Life, Average_Annual_Salary, Population FROM CitiesData JOIN RestaurantCities ON trim(CitiesData.CityID) = trim(RestaurantCities.ID)")
+    cur.execute("SELECT CitiesData.Quality_of_Life, CitiesData.Average_Annual_Salary, CitiesData.Population FROM CitiesData JOIN RestaurantCities ON trim(CitiesData.CityID) = trim(RestaurantCities.ID)")
     conn.commit()
     joined = cur.fetchall()
     for x in joined: 
